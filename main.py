4f311fef7cd042bc14420ffa17b18334059bc666
Jip Derksen <78497126+CubedCard@users.noreply.github.com>
2026-05-21 15:19:39 +0200

    First working version

--- main.py
+++ main.py
@@ -1,29 +1,56 @@
+import sys
+import threading
 import keyboard 
 import time
 import datetime
 
-global started 
+running = True 
 started = False
+start = datetime.datetime.now()
 
-def main():
+def format_time(t):
+    s = t.seconds
+    m = (s // 60) % 60
+    ms = int(t.total_seconds() * 100) - (s * 100)
+    result = str()
+    if m > 0:
+        result += f'{m}:'
+    result += f'{s}:{ms}'
+    return result
+
+def timer():
+    while running:
+        if started:
+            t = datetime.datetime.now() - start
+            time_str = format_time(t)
+            sys.stdout.write(f'\r{time_str}')
+            sys.stdout.flush()
+        time.sleep(0.01)
+
+def listen():
     while True:
+        global started
+        global start
+
         try:
             if keyboard.is_pressed('space'):
-                print('\rYou pressed space mate!! ;)')
-                global started
                 started = not started
+                if started:
+                    start = datetime.datetime.now()
                 time.sleep(0.1)
             if keyboard.is_pressed('q'):
                 print('See ya!')
                 break
         except Exception as e: # TODO: do something with the exit status
+            running = False
             print(e)
             print('Make sure to use `sudo` when running this program.')
             break
-        start = datetime.datetime.now()
-        while started:
-            print(f'\r{datetime.datetime.now() - start}')
-            time.sleep(0.01)
+
+def main():
+    thread = threading.Thread(target=timer, daemon=True)
+    thread.start()
+    listen()
 
 if __name__ == '__main__':
     main()
