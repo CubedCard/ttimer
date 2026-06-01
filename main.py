f2efbf04151d4e202a07458ac405b6578426a858
Jip Derksen <78497126+CubedCard@users.noreply.github.com>
2026-06-01 12:33:52 +0200

    Added simple score logging

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import sys
 import threading
-import keyboard 
+import keyboard
 import time
 import datetime
 
-running = True 
+running = True
 started = False
+t = 0
 start = datetime.datetime.now()
+
+
+def write_time():
+    with open("scores.txt", "a", encoding="utf-8") as f:
+        f.write(f"{t}\n")
+
 
 def format_time(t):
     s = t.seconds
@@ -14,18 +21,21 @@ def format_time(t):
     ms = int(t.total_seconds() * 100) - (s * 100)
     result = str()
     if m > 0:
-        result += f'{m}:'
-    result += f'{s}:{ms}'
+        result += f"{m}:"
+    result += f"{s}:{ms}"
     return result
 
+
 def timer():
+    global t
     while running:
         if started:
             t = datetime.datetime.now() - start
             time_str = format_time(t)
-            sys.stdout.write(f'\r{time_str}')
+            sys.stdout.write(f"\r{time_str}")
             sys.stdout.flush()
         time.sleep(0.01)
+
 
 def listen():
     while True:
@@ -33,24 +43,28 @@ def listen():
         global start
 
         try:
-            if keyboard.is_pressed('space'):
+            if keyboard.is_pressed("space"):
                 started = not started
                 if started:
                     start = datetime.datetime.now()
+                else:
+                    write_time()
                 time.sleep(0.1)
-            if keyboard.is_pressed('q'):
-                print('See ya!')
+            if keyboard.is_pressed("q"):
+                print("See ya!")
                 break
-        except Exception as e: # TODO: do something with the exit status
+        except Exception as e:  # TODO: do something with the exit status
             running = False
             print(e)
-            print('Make sure to use `sudo` when running this program.')
+            print("Make sure to use `sudo` when running this program.")
             break
+
 
 def main():
     thread = threading.Thread(target=timer, daemon=True)
     thread.start()
     listen()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
